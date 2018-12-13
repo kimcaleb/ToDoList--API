9ec2051f216d25fd69dcd1be2e2613343a06ff22
Caleb Kim <calebkim2005@yahoo.com>
2018-12-12 17:20:11 -0800

    fixed create

--- public/js/main.js
+++ public/js/main.js
@@ -6,16 +6,16 @@
 var submit = document.getElementById("submit");
 submit.addEventListener("click", (evt)=>{
     evt.preventDefault();
-    let inputContent = document.getElementById("input").innerText;
+    let inputContent = document.getElementById("input").value;
     if (inputContent === "") {alert("Please write task in the input field");}
-    else {axios.post("/api/todolists",{
+    else {axios.post("/api/todolists", {
             name: `${inputContent}`,
             isComplete: false
         }).then(function() {
             $(".listcontainer").append(
             `<div class="container">
                 <div class="row">
-                    <div class="col-4"><ul><li>${inputContent.innerText}</li></ul></div>
+                    <div class="col-4"><ul><li>${inputContent}</li></ul></div>
                     <div class="col-4"><button type="button" class="btn btn-outline-secondary btn-lg">INCOMPLETE</button><button type="button" class="btn btn-outline-danger btn-lg">DELETE</button></div>        
                 </div>
             </div>`
